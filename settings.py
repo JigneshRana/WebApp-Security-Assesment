ce3b474488acb555950dd90393d1ba8b8d095a76
JigneshRana <jimmyrana.tech@gmail.com>
2021-04-29 12:32:20 +0530

    change path

--- settings.py
+++ settings.py
@@ -1,1 +1,1 @@
-options={"LogPath":"/home/jignesh/Script/phpScanner21/Capture21/logs/","Loguid":1000,"Loggid":1000}
+options={"LogPath":"/home/jignesh/Script/phpScanner21/WebApp-Security-Assesment/logs/","Loguid":1000,"Loggid":1000}
